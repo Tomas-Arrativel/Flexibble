9af9a7e1a0a5d2b84ccdaf4c1473c565f9e21081
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-05 11:06:26 -0300

    session and api

---
+++ lib/session.ts
@@ -0,0 +1,41 @@
+import { getServerSession } from 'next-auth/next';
+import { NextAuthOptions, User } from 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
+import GoogleProvider from 'next-auth/providers/google';
+import jsonwebtoken from 'jsonwebtoken';
+import { JWT } from 'next-auth/jwt';
+import { SessionInterface } from '@/common.types';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  //   jwt: {
+  //     encode: ({ secret, token }) => {},
+  //     decode: ({ secret, token }) => {},
+  //   },
+  theme: { colorScheme: 'light', logo: '/logo.png' },
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        // get user if exists
+        //if don't we gonna create it
+        return true;
+      } catch (error: any) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+  return session;
+}
