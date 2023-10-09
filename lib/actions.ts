0e584e422d538308a11ae712dc5bf0657718c6c7
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-09 10:52:10 -0300

    df

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
+const isProduction = (process.env.NODE_ENV = 'production');
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ''
+  : 'https://';
+
 const client = new GraphQLClient('apiURl');
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
